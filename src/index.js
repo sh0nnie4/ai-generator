b5c7e707e1becd529281ca1a62748b7468cbddbd
sh0nnie4 <139727327+sh0nnie4@users.noreply.github.com>
2024-02-24 16:24:29 -0500

    Added basic form handling and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: "Quote goes here",
+    autoStart: true,
+    cursor: "",
+    delay: 1,
+  });
+}
+
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
