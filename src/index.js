6866a83c79716a8a1f45905b90995d357f93fbe8
sh0nnie4 <139727327+sh0nnie4@users.noreply.github.com>
2024-02-24 17:52:58 -0500

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-function generateQuote(event) {
-  event.preventDefault();
-
+function displayquote(response) {
   new Typewriter("#quote", {
-    strings: "Quote goes here",
+    strings: response.data.answer,
     autoStart: true,
     cursor: "",
     delay: 1,
   });
+}
+
+function generateQuote(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "9aa8dte54640eadb9ed0cb3f8cboafa2";
+
+  let context =
+    "You are a quote expert in every topic and love to write short quotes. Your mission is to generate a quote in bold and quotations in basic HTML. Make sure to follow the user instructions.";
+  let prompt = `User instructions: Generate a quote about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayquote);
 }
 
 let quoteFormElement = document.querySelector("#quote-generator-form");
